62f6424a9529ff26d0c0c3c859408961987669f3
nguyenminhduc94 <>
2017-11-16 17:08:23 +0700

    add .

---
+++ Assets/Scripts/SpawnerManager/ScoreScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreScript : MonoBehaviour {
+
+	// Use this for initialization
+	public float forceX;
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 vc3 = transform.position;
+		vc3.x -= forceX * Time.deltaTime;
+		transform.position = vc3;
+	}
+
+	void OnTriggerEnter2D(Collider2D target){
+		if(target.tag == "Player"){
+			Destroy (gameObject);
+		}
+	}
+}
---
+++ Assets/Scripts/SpawnerManager/SpawnerScore.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnerScore : MonoBehaviour {
+
+	[SerializeField]
+	private GameObject score;
+	// Use this for initialization
+	void Start () {
+		StartCoroutine (Clone());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	IEnumerator Clone(){
+		yield return new WaitForSeconds (1);
+		Instantiate (score, transform.position, Quaternion.identity);
+		StartCoroutine (Clone());
+	}
+}
